37c295ac85bfe0ca4753561fa563008db7d21678
Nico <nporsch@ethz.ch>
2024-10-28 16:28:03 +0100

    changes in PS2 3b

--- PS2/ProblemSet2_3_b.py
+++ PS2/ProblemSet2_3_b.py
@@ -61,108 +61,122 @@ for player_points in range(0,4+1):
             if not (player_points==4 or opponent_points==4):
                 states.add(State(player_points, opponent_points, 2))
 
-#Transition probabilities
-def getProb(state_from: State, state_to: State, p: float, q: float, winning_states: set[State], losing_states: set[State]) -> float:    
-    #Terminal states
-    if (state_from in winning_states or state_from in losing_states) and state_from == state_to: 
-        return 1
-    #Win point on either serve
-    if state_to.player_points == state_from.player_points + 1 and state_to.opponent_points == state_from.opponent_points and state_to.serve == 1: 
-        return p * q
-    #Win point on either serve on deuce
-    if state_to.player_points == 3 and state_from.player_points == 3 and state_to.opponent_points == 2 and state_from.opponent_points == 3 and state_to.serve == 1: 
-        return p * q
-    # first serve in net
-    if state_to.player_points == state_from.player_points and state_to.opponent_points == state_from.opponent_points and state_to.serve == state_from.serve + 1: 
-        return 1 - p
-    # Lose point on first serve
-    if state_to.player_points == state_from.player_points and state_to.opponent_points == state_from.opponent_points + 1 and state_from.serve == 1 and state_to.serve == 1: 
-        return p * (1 - q)
-    # Lose point on first serve at deuce
-    if state_to.player_points == 2 and state_from.player_points == 3 and state_to.opponent_points == 3 and state_from.opponent_points == 3 and state_to.serve == 1:
-        return p * (1 - q)
-    # Lose point on second serve
-    if state_to.player_points == state_from.player_points and state_to.opponent_points == state_from.opponent_points + 1 and state_from.serve == 2 and state_to.serve == 1: 
-        return 1 - p * q
-    # Lose point on second serve at deuce
-    if state_to.player_points == 2 and state_from.player_points == 3 and state_to.opponent_points == 3 and state_from.opponent_points == 3 and state_from.serve == 2 and state_to.serve == 1:
-        return 1 - p * q
-    return 0
 
 #Actions            
 class Serve(Enum):
     FAST = 0
     SLOW = 1
 
-# Landing probability
-pF = 0.55 # fast serve
-pS = 0.95  # slow serve
-# Winning probability
-qF = 0.6  # fast serve
-qS = 0.4  # slow serve
+prob_win: dict[float, float] = {}
 
+for pF in (0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1):
+    # Landing probability
+    pF = pF # fast serve
+    pS = 0.95  # slow serve
+    # Winning probability
+    qF = 0.6  # fast serve
+    qS = 0.4  # slow serve
 
-#Value iteration
-value: dict[State, float] = {}
-
-max_diff = np.inf
-it = 0
-while max_diff > 1e-8 and it < 1000:
-    value_old = value.copy()
+    #Calculate transition probabilities
+    probs: dict[(State, Serve, State), float] = {}
     for state_from in states:
-        max_value = -np.inf
-        for action in {Serve.FAST, Serve.SLOW}:
-            action_value = 0
-            if action == Serve.FAST:
-                p: float = pF
-                q: float  = qF
-            else: # action == Serve.SLOW
-                p: float  = pS
-                q: float  = qS
-            for state_to in states:
-                prob = getProb(state_from, state_to, p, q, winning_states, losing_states) #Probabilities
-                rew = 0
-                if state_to in winning_states and not state_from in winning_states:
-                    rew = 1
-                action_value += prob * (rew + 1*value.get(state_to, 0))
-            if action_value > max_value:
-                max_value = action_value
-        value[state_from] = max_value
-    
-    #termination condition
-    max_diff = 0
+            for action in {Serve.FAST, Serve.SLOW}:
+                if action == Serve.FAST:
+                    p: float = pF
+                    q: float  = qF
+                else: # action == Serve.SLOW
+                    p: float  = pS
+                    q: float  = qS
+                for state_to in states:
+                    #Terminal states
+                    if (state_from in winning_states or state_from in losing_states) and state_from == state_to: 
+                        probs[(state_from, action, state_to)] = 1
+                    #Win point on either serve
+                    elif state_to.player_points == state_from.player_points + 1 and state_to.opponent_points == state_from.opponent_points and state_to.serve == 1: 
+                        probs[(state_from, action, state_to)] = p * q
+                    #Win point on either serve on deuce
+                    elif state_to.player_points == 3 and state_from.player_points == 3 and state_to.opponent_points == 2 and state_from.opponent_points == 3 and state_to.serve == 1: 
+                        probs[(state_from, action, state_to)] = p * q
+                    # first serve in net
+                    elif state_to.player_points == state_from.player_points and state_to.opponent_points == state_from.opponent_points and state_to.serve == state_from.serve + 1: 
+                        probs[(state_from, action, state_to)] = 1 - p
+                    # Lose point on first serve
+                    elif state_to.player_points == state_from.player_points and state_to.opponent_points == state_from.opponent_points + 1 and state_from.serve == 1 and state_to.serve == 1: 
+                        probs[(state_from, action, state_to)] = p * (1 - q)
+                    # Lose point on first serve at deuce
+                    elif state_to.player_points == 2 and state_from.player_points == 3 and state_to.opponent_points == 3 and state_from.opponent_points == 3 and state_to.serve == 1:
+                        probs[(state_from, action, state_to)] = p * (1 - q)
+                    # Lose point on second serve
+                    elif state_to.player_points == state_from.player_points and state_to.opponent_points == state_from.opponent_points + 1 and state_from.serve == 2 and state_to.serve == 1: 
+                        probs[(state_from, action, state_to)] = 1 - p * q
+                    # Lose point on second serve at deuce
+                    elif state_to.player_points == 2 and state_from.player_points == 3 and state_to.opponent_points == 3 and state_from.opponent_points == 3 and state_from.serve == 2 and state_to.serve == 1:
+                        probs[(state_from, action, state_to)] = 1 - p * q
+
+    #Value iteration
+    value: dict[State, float] = {}
+    max_diff = np.inf
+    it = 0
+    while max_diff > 1e-8 and it < 1000:
+        value_old = value.copy()
+        for state_from in states:
+            max_value = -np.inf
+            for action in {Serve.FAST, Serve.SLOW}:
+                action_value = 0
+                if action == Serve.FAST:
+                    p: float = pF
+                    q: float  = qF
+                else: # action == Serve.SLOW
+                    p: float  = pS
+                    q: float  = qS
+                for state_to in states:
+                    prob = probs.get((state_from, action, state_to), 0) 
+                    rew = 0
+                    if state_to in winning_states and not state_from in winning_states:
+                        rew = 1
+                    action_value += prob * (rew + 1*value.get(state_to, 0))
+                if action_value > max_value:
+                    max_value = action_value
+            value[state_from] = max_value
+        
+        #termination condition
+        max_diff = 0
+        for state in states:
+            max_diff = max(max_diff, np.abs(value.get(state, 10) - value_old.get(state, 0)))
+        it += 1
+        #print(f"Iteration {it} max_diff: {max_diff}")
+
+    #Optimal policy
+    optimal_serve: dict[State, Serve] = {}
+    for state_from in states:
+            max_value = -np.inf
+            for action in {Serve.FAST, Serve.SLOW}:
+                action_value = 0
+                if action == Serve.FAST:
+                    p: float = pF
+                    q: float  = qF
+                else: # action == Serve.SLOW
+                    p: float  = pS
+                    q: float  = qS
+                for state_to in states:
+                    prob = probs.get((state_from, action, state_to), 0)
+                    rew = 0
+                    if state_to in losing_states:
+                        rew = -1
+                    action_value += prob * (rew + 0.9*value.get(state_to, 0))
+                if action_value > max_value:
+                    max_value = action_value
+                    optimal_serve[state_from] = action
+
+    prob_win[pF] = value.get(State(0,0,1), 0)
+    #Print results
     for state in states:
-        max_diff = max(max_diff, np.abs(value.get(state, 10) - value_old.get(state, 0)))
-    it += 1
-    #print(f"Iteration {it} max_diff: {max_diff}")
+        if state in winning_states:
+            value[state] = 1
 
-optimal_serve: dict[State, Serve] = {}
+    #for state in states:
+    #    print(f"{state} {optimal_serve.get(state, None)} | Winning Probabibility: {value.get(state, 0)}")
 
-for state_from in states:
-        max_value = -np.inf
-        for action in {Serve.FAST, Serve.SLOW}:
-            action_value = 0
-            if action == Serve.FAST:
-                p: float = pF
-                q: float  = qF
-            else: # action == Serve.SLOW
-                p: float  = pS
-                q: float  = qS
-            for state_to in states:
-                prob = getProb(state_from, state_to, p, q, winning_states, losing_states) #Probabilities
-                rew = 0
-                if state_to in losing_states:
-                    rew = -1
-                action_value += prob * (rew + 0.9*value.get(state_to, 0))
-            if action_value > max_value:
-                max_value = action_value
-                optimal_serve[state_from] = action
-
-for state in states:
-    if state in winning_states:
-        value[state] = 1
-
-for state in states:
-    print(f"{state} {optimal_serve.get(state, None)} | Winning Probabibility: {value.get(state, 0)}")
-
-
+print(prob_win)
+#plt.plot(list(prob_win.keys()), list(prob_win.values()))
+#plt.show()
